dcfd5546b95b0bc940e2abe41e6451f151fa4583
FlakoBB <josephrbb99@gmail.com>
2023-10-04 14:12:06 -0600

    add logo and icon

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,8 @@ export default function Home () {
     <div>
       <h1>Source Code: Homepage</h1>
       <p>La Newsletter de tecnología.</p>
+      <img src='/LogoSB.png' alt='Logo Oficial de SourceBug en color azul' />
+      <img src='/IconoSB.png' alt='Icono Oficial de SourceBug en color azul' />
     </div>
   )
 }
